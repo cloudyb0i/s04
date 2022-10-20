e6f361a1bd5cfc60a6b01c8a259b4ae711744216
cloudyb0i <107659931+cloudyb0i@users.noreply.github.com>
2022-10-20 17:23:40 +0800

    Add files via upload
    aggregation

---
+++ activity-aggregation.js
@@ -0,0 +1,242 @@
+/* 1 */
+{
+    "_id" : 345342.0,
+    "employeeName" : "Joongkuk Jeon",
+    "employeeAge" : 25.0,
+    "jobRole" : "Store Associate",
+    "Salary" : 45000.0
+}
+
+/* 2 */
+{
+    "_id" : 312456.0,
+    "employeeName" : "Joongki Song",
+    "employeeAge" : 28.0,
+    "jobRole" : "Store Manager",
+    "Salary" : 120000.0
+}
+
+/* 3 */
+{
+    "_id" : 334566.0,
+    "employeeName" : "Jin Kim",
+    "employeeAge" : 35.0,
+    "jobRole" : "Cashier",
+    "Salary" : 67500.0
+}
+
+/* 4 */
+{
+    "_id" : 245345.0,
+    "employeeName" : "Hyekyo Song",
+    "employeeAge" : 40.0,
+    "jobRole" : "Senior Cashier",
+    "Salary" : 72500.0
+}
+
+/* 5 */
+{
+    "_id" : 445364.0,
+    "employeeName" : "Tae Hyung Kim",
+    "employeeAge" : 22.0,
+    "jobRole" : "Store Associate",
+    "Salary" : 35000.0
+}
+
+/* 1 */
+{
+    "_id" : 102.0,
+    "name" : "Banana",
+    "supplier" : "Yellow Co.",
+    "stock" : 15.0,
+    "price" : 20.0,
+    "onSale" : true
+}
+
+/* 2 */
+{
+    "_id" : 103.0,
+    "name" : "Kiwi",
+    "supplier" : "Green Farms",
+    "stock" : 25.0,
+    "price" : 50.0,
+    "onSale" : true
+}
+
+/* 3 */
+{
+    "_id" : 101.0,
+    "name" : "Apple",
+    "supplier" : "Red Farms Inc.",
+    "stock" : 20.0,
+    "price" : 20.0,
+    "onSale" : true
+}
+
+/* 4 */
+{
+    "_id" : 104.0,
+    "name" : "Mango",
+    "supplier" : "Yellow Co.",
+    "stock" : 10.0,
+    "price" : 120.0,
+    "onSale" : false
+}
+
+/* 1 */
+{
+    "_id" : "LS0000123",
+    "Name" : "XYZ Chocolate Bar - 100g",
+    "Price" : 5.23,
+    "Quantity" : 25000.0,
+    "Category" : "chocolates, sweets"
+}
+
+/* 2 */
+{
+    "_id" : "LS0003123",
+    "Name" : "Milk Non-fat - 1lt",
+    "Price" : 3.0,
+    "Quantity" : 1000.0,
+    "Category" : "dairy, healthy"
+}
+
+/* 3 */
+{
+    "_id" : "LS0009100",
+    "Name" : "Bell Pepper (Packed) - 250 g",
+    "Price" : 4.95,
+    "Quantity" : 12000.0,
+    "Category" : "vegetables, healthy, organic"
+}
+
+/* 4 */
+{
+    "_id" : "LS0002688",
+    "Name" : "ZZ Butter - 500 g",
+    "Price" : 25.0,
+    "Quantity" : 500.0,
+    "Category" : "dairy, healthy, premium"
+}
+
+/* 5 */
+{
+    "_id" : "LS0009846",
+    "Name" : "Beans (Packed) - 250g",
+    "Price" : 6.75,
+    "Quantity" : 6000.0,
+    "Category" : "vegetables, healthy, organic"
+}
+
+/* 6 */
+{
+    "_id" : "LS0004566",
+    "Name" : "Eggs - 12 Pack",
+    "Price" : 6.0,
+    "Quantity" : 5000.0,
+    "Category" : "poultry, generic"
+}
+
+/* 7 */
+{
+    "_id" : "LS0008542",
+    "Name" : "Carrots (Packed) - 250g",
+    "Price" : 3.5,
+    "Quantity" : 3000.0,
+    "Category" : "vegetables, healthy, organic"
+}
+
+/* 8 */
+{
+    "_id" : "LS0008543",
+    "Name" : "Carrots (Packed) - 250g",
+    "Price" : 3.5,
+    "Quantity" : 3000.0,
+    "Category" : "vegetables, healthy, organic"
+}
+
+/* 1 */
+{
+    "_id" : "BL2021005",
+    "grossAmount" : 105.65,
+    "Discounts" : 10.0,
+    "netAmount" : 95.65,
+    "dateTime" : ISODate("2021-01-01T16:00:00.000Z")
+}
+
+/* 2 */
+{
+    "_id" : "BL2021010",
+    "grossAmount" : 33.5,
+    "Discounts" : 20.5,
+    "netAmount" : 13.0,
+    "dateTime" : "2021-01-01T11:02:35Z"
+}
+
+/* 3 */
+{
+    "_id" : "BL2021009",
+    "grossAmount" : 89.72,
+    "Discounts" : 0.72,
+    "netAmount" : 89.0,
+    "dateTime" : ISODate("2021-01-01T08:45:12.000Z")
+}
+
+/* 4 */
+{
+    "_id" : "BL2021007",
+    "grossAmount" : 153.33,
+    "Discounts" : 20.33,
+    "netAmount" : 133.0,
+    "dateTime" : ISODate("2021-01-01T16:31:08.000Z")
+}
+
+/* 5 */
+{
+    "_id" : "BL2021006",
+    "grossAmount" : 45.25,
+    "Discounts" : 0.0,
+    "netAmount" : 45.25,
+    "dateTime" : ISODate("2021-01-01T16:15:55.000Z")
+}
+
+/* 6 */
+{
+    "_id" : "BL2021008",
+    "grossAmount" : 21.0,
+    "Discounts" : 0.0,
+    "netAmount" : 21.0,
+    "dateTime" : ISODate("2021-01-01T20:25:52.000Z")
+}
+
+/* 1 */
+{
+    "_id" : "PROMO04",
+    "Name" : "New Year Promo",
+    "Period" : 7.0,
+    "dailySales" : "68, 88, 105, 188, 74, 278, 350"
+}
+
+/* 2 */
+{
+    "_id" : "PROMO01",
+    "Name" : "Sales Promo",
+    "Period" : 7.0,
+    "dailySales" : "20, 50, 12, 30, 45, 15, 60"
+}
+
+/* 3 */
+{
+    "_id" : "PROMO03",
+    "Name" : "Meat Promo",
+    "Period" : 3.0,
+    "dailySales" : "101, 205"
+}
+
+/* 4 */
+{
+    "_id" : "PROMO02",
+    "Name" : "Milk Promo",
+    "Period" : 2.0,
+    "dailySales" : "120, 200"
+}
